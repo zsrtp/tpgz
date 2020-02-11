8ef03069c23d0355763393bbbabdfdb48bc9ed1f
unknown <pheenoh@gmail.com>
2020-02-11 00:17:42 -0500

    prelim support for gorge void indicator

--- src/cheats.cpp
+++ src/cheats.cpp
@@ -14,37 +14,36 @@ namespace Cheats {
     static bool got_it = false;
 
     void apply_cheats() {
-        if (tp_gameInfo.freeze_game != 0 && strcmp((const char *)tp_gameInfo.current_stage, "F_SP121") == 0 && tp_gameInfo.cs_val == 0x128) {
+        
+        if (tp_gameInfo.freeze_game == 1 && tp_gameInfo.cs_val == 0x128 && strcmp((const char *)tp_gameInfo.current_stage, "F_SP121") == 0) {
             before_cs_val++;
-        };
-
-        if (tp_gameInfo.freeze_game != 0 && strcmp((const char *)tp_gameInfo.current_stage, "F_SP121") == 0 && tp_gameInfo.cs_val == 0x900) {
+        }
+        else if (before_cs_val == 132 && (tp_gameInfo.freeze_game == 2 || tp_gameInfo.cs_val == 0x900) && strcmp((const char *)tp_gameInfo.current_stage, "F_SP121") == 0) {
             after_cs_val++;
-        };
-        if (before_cs_val >= 104) {
-            
+        }
+
+        if (before_cs_val > 122 && after_cs_val < 10) {
+            // only report if you didn't hold it last frame
             if (tp_mPadStatus.sval != (Pad::L | Pad::A)) { 
                 held_last_frame = false;
             }
 
-            if (got_it == false && held_last_frame == false && (before_cs_val <= 134) && tp_gameInfo.cs_val == 0x128 && tp_gameInfo.freeze_game != 2 && tp_mPadStatus.sval == (Pad::L | Pad::A)) {
+            if (got_it == false && held_last_frame == false && before_cs_val < 132 && tp_mPadStatus.sval == (Pad::L | Pad::A)) {
                 // went early
-                int final_val = 135 - before_cs_val;
-                held_last_frame = true;
+                int final_val = 132 - before_cs_val;
                 tp_osReport("%df early", final_val);
-                tp_osReport("%df late", after_cs_val);
+                held_last_frame = true;
                 Controller::set_buttons_down(0x0);
                 Controller::set_buttons_pressed(0x0);
                 tp_mPadButton.sval = 0x0;
                 tp_mPadStatus.sval = 0x0;
             }
 
-            else if (got_it == false && held_last_frame == false && (before_cs_val == 134) && tp_gameInfo.freeze_game == 2 && tp_mPadStatus.sval == (Pad::L | Pad::A)) {
+            else if (got_it == false && held_last_frame == false && before_cs_val == 132 && 
+            tp_mPadStatus.sval == (Pad::L | Pad::A) && after_cs_val == 0) {
                 // got it
                 held_last_frame = true;
                 tp_osReport("<3");
-                tp_osReport("%df early", before_cs_val);
-                tp_osReport("%df late", after_cs_val);
                 Controller::set_buttons_down(0x0);
                 Controller::set_buttons_pressed(0x0);
                 tp_mPadButton.sval = 0x0;
@@ -52,31 +51,26 @@ namespace Cheats {
                 got_it = true;
             }
 
-            else if (got_it == false && held_last_frame == false && (after_cs_val <= 30) && tp_mPadStatus.sval == (Pad::L | Pad::A)) {
+            else if (got_it == false && held_last_frame == false && after_cs_val >0 && tp_mPadStatus.sval == (Pad::L | Pad::A)) {
                 // went late
-                // sloppy
-                if (tp_gameInfo.freeze_game == 1) { after_cs_val = 1;}
-                held_last_frame = true;
-                tp_osReport("%df early", before_cs_val + 1);
                 tp_osReport("%df late", after_cs_val);
+                held_last_frame = true;
                 Controller::set_buttons_down(0x0);
                 Controller::set_buttons_pressed(0x0);
                 tp_mPadButton.sval = 0x0;
                 tp_mPadStatus.sval = 0x0;
             }
 
-            else { }
-        }
-
-        if (tp_mPadStatus.sval == (Pad::R | Pad::A)) {
-            if (tp_gameInfo.momentum_ptr) {
-                tp_gameInfo.momentum_ptr->link_momentum.y = 56.0f;
+            if (tp_mPadStatus.sval == (Pad::R | Pad::A)) {
+                if (tp_gameInfo.momentum_ptr) {
+                    tp_gameInfo.momentum_ptr->link_momentum.y = 56.0f;
+                }
+                Inventory::set_rupee_flags();
+                Controller::set_buttons_down(0x0);
+                Controller::set_buttons_pressed(0x0);
+                tp_mPadButton.sval = 0x0;
+                tp_mPadStatus.sval = 0x0;
             }
-            Inventory::set_rupee_flags();
-            Controller::set_buttons_down(0x0);
-            Controller::set_buttons_pressed(0x0);
-            tp_mPadButton.sval = 0x0;
-            tp_mPadStatus.sval = 0x0;
         }
     }
 }  // namespace Cheats
