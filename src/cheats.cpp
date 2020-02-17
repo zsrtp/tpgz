4287a97250f15deba46ba1369306dad8172ed276
unknown <pheenoh@gmail.com>
2020-02-16 19:56:12 -0500

    add roll indicator cheat

--- src/cheats.cpp
+++ src/cheats.cpp
@@ -12,12 +12,12 @@ namespace Cheats {
 
     static int before_cs_val = 0;
     static int after_cs_val = 0;
-    static int roll_frames = 25;
     static int roll_counter = 0;
+    static int missed_counter = 0;
     static bool held_last_frame = false;
     static bool got_it = false;
 
-    const int CHEAT_AMNT = 12;
+    const int CHEAT_AMNT = 13;
 
     enum CheatId {
         Invincible,
@@ -31,7 +31,8 @@ namespace Cheats {
         TeleportEnabled,
         ReloadArea,
         FastRolling,
-        GorgeVoid
+        GorgeVoid,
+        RollCheck
     };
 
     struct Cheat {
@@ -53,7 +54,8 @@ namespace Cheats {
         {TeleportEnabled, "Teleport Enabled", true},
         {ReloadArea, "Reload Area", true},
         {FastRolling, "Fast Rolling", true},
-        {GorgeVoid, "Gorge Void Indicator", true}};
+        {GorgeVoid, "Gorge Void Indicator", true},
+        {RollCheck, "Roll Indicator", true}};
 
     void moon_jump() {
         if (tp_gameInfo.momentum_ptr) {
@@ -78,25 +80,96 @@ namespace Cheats {
     };
 
     void roll_check() {
-        // add animation check it will make this logic easier
-        if (tp_gameInfo.freeze_game == 0) {
+        // if normal human link gameplay
+        if (tp_gameInfo.freeze_game == 0 && tp_gameInfo.link.is_wolf == false) {
             if (tp_mPadStatus.sval != (Pad::A)) {
                 held_last_frame = false;
             }
-            if (tp_mPadStatus.sval == Pad::A) {
-                if (roll_counter < roll_frames) {
-                    tp_osReport("%df early", roll_frames - roll_counter);
-                    held_last_frame = true;
-                } else if (roll_counter == roll_frames) {
-                    tp_osReport("<3", roll_frames - roll_counter);
-                    held_last_frame = true;
-                } else {
-                    tp_osReport("%df late", roll_counter - roll_frames);
+            // check if went early or got it
+            if (tp_zelAudio.link_debug_ptr->current_action_id == 14) {
+                // reset counter cause next frame is either a new roll or a miss
+                if (roll_counter > 20) {
                     roll_counter = 0;
-                };
-            } else {
+                }
                 roll_counter++;
-            };
+                if (missed_counter > 0 && held_last_frame == false) {
+                    tp_osReport("%df late", missed_counter);
+                    missed_counter = 0;
+                } else {
+                    if (roll_counter > 15 && roll_counter < 20 && tp_mPadStatus.sval == Pad::A && held_last_frame == false) {
+                        tp_osReport("%df early", 20 - roll_counter);
+                        held_last_frame = true;
+                    } else if (roll_counter == 20 && tp_mPadStatus.sval == Pad::A && held_last_frame == false) {
+                        tp_osReport("<3");
+                        held_last_frame = true;
+                    }
+                }
+            } else if (roll_counter > 0 || missed_counter > 0) {
+                missed_counter++;
+                if (missed_counter > 5) {
+                    missed_counter = 0;
+                }
+            }
+            // account for roll interupt
+            if (roll_counter > 0 && roll_counter < 21 && tp_zelAudio.link_debug_ptr->current_action_id != 14) {
+                roll_counter = 0;
+            }
+        }
+    }
+
+    void gorge_void() {
+        if (tp_gameInfo.freeze_game == 1 && tp_gameInfo.cs_val == 0x128 && strcmp((const char *)tp_gameInfo.current_stage, "F_SP121") == 0) {
+            before_cs_val++;
+        } else if (before_cs_val == 132 && (tp_gameInfo.freeze_game == 2 || tp_gameInfo.cs_val == 0x900) && strcmp((const char *)tp_gameInfo.current_stage, "F_SP121") == 0) {
+            after_cs_val++;
+        }
+
+        if (before_cs_val > 122 && after_cs_val < 10) {
+            // only report if you didn't hold it last frame
+            if (tp_mPadStatus.sval != (Pad::L | Pad::A)) {
+                held_last_frame = false;
+            }
+
+            if (got_it == false && held_last_frame == false && before_cs_val < 132 && tp_mPadStatus.sval == (Pad::L | Pad::A)) {
+                // went early
+                int final_val = 132 - before_cs_val;
+                tp_osReport("%df early", final_val);
+                held_last_frame = true;
+                Controller::set_buttons_down(0x0);
+                Controller::set_buttons_pressed(0x0);
+                tp_mPadButton.sval = 0x0;
+                tp_mPadStatus.sval = 0x0;
+            }
+
+            else if (got_it == false && held_last_frame == false && before_cs_val == 132 &&
+                     tp_mPadStatus.sval == (Pad::L | Pad::A) && after_cs_val == 0) {
+                // got it
+                held_last_frame = true;
+                tp_osReport("<3");
+                Controller::set_buttons_down(0x0);
+                Controller::set_buttons_pressed(0x0);
+                tp_mPadButton.sval = 0x0;
+                tp_mPadStatus.sval = 0x0;
+                got_it = true;
+            }
+
+            else if (got_it == false && held_last_frame == false && after_cs_val > 0 && tp_mPadStatus.sval == (Pad::L | Pad::A)) {
+                // went late
+                tp_osReport("%df late", after_cs_val);
+                held_last_frame = true;
+                Controller::set_buttons_down(0x0);
+                Controller::set_buttons_pressed(0x0);
+                tp_mPadButton.sval = 0x0;
+                tp_mPadStatus.sval = 0x0;
+            }
+
+            if (tp_mPadStatus.sval == (Pad::R | Pad::A)) {
+                Inventory::set_rupee_flags();
+                Controller::set_buttons_down(0x0);
+                Controller::set_buttons_pressed(0x0);
+                tp_mPadButton.sval = 0x0;
+                tp_mPadStatus.sval = 0x0;
+            }
         }
     }
 
@@ -105,8 +178,6 @@ namespace Cheats {
     void load_position(){};
 
     void apply_cheats() {
-        roll_check();
-
         Link::Link link = *Link::get_link();
         Inventory::Inventory inventory = *Inventory::get_inventory();
         for (auto cheat : Items) {
@@ -147,62 +218,13 @@ namespace Cheats {
                         Cheats::Commands[RELOAD_AREA].active = true;
                     }
                     case FastRolling: {
-                        tp_linkRollConstants.roll_factor = 3.0;
+                        //tp_linkRollConstants.roll_factor = 3.0;
+                    }
+                    case RollCheck: {
+                        roll_check();
                     }
                     case GorgeVoid: {
-                        if (tp_gameInfo.freeze_game == 1 && tp_gameInfo.cs_val == 0x128 && strcmp((const char *)tp_gameInfo.current_stage, "F_SP121") == 0) {
-                            before_cs_val++;
-                        } else if (before_cs_val == 132 && (tp_gameInfo.freeze_game == 2 || tp_gameInfo.cs_val == 0x900) && strcmp((const char *)tp_gameInfo.current_stage, "F_SP121") == 0) {
-                            after_cs_val++;
-                        }
-
-                        if (before_cs_val > 122 && after_cs_val < 10) {
-                            // only report if you didn't hold it last frame
-                            if (tp_mPadStatus.sval != (Pad::L | Pad::A)) {
-                                held_last_frame = false;
-                            }
-
-                            if (got_it == false && held_last_frame == false && before_cs_val < 132 && tp_mPadStatus.sval == (Pad::L | Pad::A)) {
-                                // went early
-                                int final_val = 132 - before_cs_val;
-                                tp_osReport("%df early", final_val);
-                                held_last_frame = true;
-                                Controller::set_buttons_down(0x0);
-                                Controller::set_buttons_pressed(0x0);
-                                tp_mPadButton.sval = 0x0;
-                                tp_mPadStatus.sval = 0x0;
-                            }
-
-                            else if (got_it == false && held_last_frame == false && before_cs_val == 132 &&
-                                     tp_mPadStatus.sval == (Pad::L | Pad::A) && after_cs_val == 0) {
-                                // got it
-                                held_last_frame = true;
-                                tp_osReport("<3");
-                                Controller::set_buttons_down(0x0);
-                                Controller::set_buttons_pressed(0x0);
-                                tp_mPadButton.sval = 0x0;
-                                tp_mPadStatus.sval = 0x0;
-                                got_it = true;
-                            }
-
-                            else if (got_it == false && held_last_frame == false && after_cs_val > 0 && tp_mPadStatus.sval == (Pad::L | Pad::A)) {
-                                // went late
-                                tp_osReport("%df late", after_cs_val);
-                                held_last_frame = true;
-                                Controller::set_buttons_down(0x0);
-                                Controller::set_buttons_pressed(0x0);
-                                tp_mPadButton.sval = 0x0;
-                                tp_mPadStatus.sval = 0x0;
-                            }
-
-                            if (tp_mPadStatus.sval == (Pad::R | Pad::A)) {
-                                Inventory::set_rupee_flags();
-                                Controller::set_buttons_down(0x0);
-                                Controller::set_buttons_pressed(0x0);
-                                tp_mPadButton.sval = 0x0;
-                                tp_mPadStatus.sval = 0x0;
-                            }
-                        }
+                        gorge_void();
                     }
                 }
             } else {
