a5c908a04cbfb7dfe5f6c37f5e32638c0f092489
TakaRikka <38417346+TakaRikka@users.noreply.github.com>
2020-08-06 01:12:56 -0700

    formatting

--- src/main_menu.cpp
+++ src/main_menu.cpp
@@ -12,15 +12,15 @@ static Cursor cursor = {0,0};
 bool mm_visible;
 
 Line lines[LINES] = {
-    {"cheats", CHEAT_INDEX, "turn cheats on/off", false},
-    {"flags", FLAGS_INDEX, "turn in game flags on/off", false},
-    {"inventory", INVENTORY_INDEX, "set link's items and equipment", false},
-    {"memory", MEMORY_INDEX, "add memory watches to the screen", false},
-    {"practice", PRACTICE_INDEX, "load practice files", false},
-    {"scene", SCENE_INDEX, "practice tools for various categories", false},
-    {"settings", SETTINGS_INDEX, "configure settings", false},
-    {"tools", TOOLS_INDEX, "various tools for practice and testing", false},
-    {"warping", WARPING_INDEX, "warp to dungeons, towns, grottos, etc.", false}};
+    {"cheats", CHEAT_INDEX, "Turn cheats on/off", false},
+    {"flags", FLAGS_INDEX, "Turn in game flags on/off", false},
+    {"inventory", INVENTORY_INDEX, "Set link's items and equipment", false},
+    {"memory", MEMORY_INDEX, "Add memory watches to the screen", false},
+    {"practice", PRACTICE_INDEX, "Load practice files", false},
+    {"scene", SCENE_INDEX, "Adjust the scene's state", false},
+    {"settings", SETTINGS_INDEX, "Configure settings", false},
+    {"tools", TOOLS_INDEX, "Use various tools for practice and testing", false},
+    {"warping", WARPING_INDEX, "Warp to dungeons, towns, grottos, etc.", false}};
 
 void MainMenu::render(Font& font) {
 
@@ -65,8 +65,8 @@ void MainMenu::render(Font& font) {
                 return;
             }
             case PRACTICE_INDEX: {
-                prac_visible = true;
                 mm_visible = false;
+                prac_visible = true;
                 return;
             }
             case SCENE_INDEX: {
