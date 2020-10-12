107a84fb8aef6dfebd6e18915d08e182937c0b0d
unknown <pheenoh@gmail.com>
2020-10-11 22:45:49 -0400

    adjust patch

--- patch.asm
+++ patch.asm
@@ -2,7 +2,7 @@
 ; lis r3, 0x8050 ; set Arena low to 0x80507be0
 ; with inject at 0x80460000 gives us 0xA7BE0 bytes
 u32 0x3c60804F
-u32 0x38637FFF
+u32 0x38631FFF
 
 ; This will hook the main() function
 0x80006454:
