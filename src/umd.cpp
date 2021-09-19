c654d5db4d835f320a3be85f989dc4677b0eab01
TakaRikka <38417346+TakaRikka@users.noreply.github.com>
2021-09-18 19:35:44 -0700

    format

--- src/umd.cpp
+++ src/umd.cpp
@@ -28,12 +28,12 @@ static const char* getPressedButtonText() {
         return "A";
     else if (!button_is_held(B, POST_GAME_LOOP) && button_is_down(B))
         return "B";
- 
+
     return "";
 }
 
 void run() {
-    //Reset everything if map is not active
+    // Reset everything if map is not active
     if (g_meter2_info.mMapStatus != 2 && g_meter2_info.mMapStatus != 3) {
         counter_difference = 0;
         firstPressedButton = -1;
@@ -44,9 +44,9 @@ void run() {
     if (g_meter2_info.mMenuWindowClass != NULL) {
         current_counter = cCt_getFrameCount();
 
-        //Activate frame counter as soon as a map opening was scheduled
-        if (start_timer == false && (g_meter2_info.mMapStatus == 2 || g_meter2_info.mMapStatus == 3)) {
-
+        // Activate frame counter as soon as a map opening was scheduled
+        if (start_timer == false &&
+            (g_meter2_info.mMapStatus == 2 || g_meter2_info.mMapStatus == 3)) {
             bool eventRunning = dMw_c__isEventCheck(g_meter2_info.mMenuWindowClass);
 
             if (!eventRunning) {
@@ -57,24 +57,22 @@ void run() {
         }
 
         if (start_timer == true && !exitCheck) {
-
             counter_difference += current_counter - previous_counter;
             previous_counter = current_counter;
 
-            //Only care up to 15f after
+            // Only care up to 15f after
             if (counter_difference < 15) {
-
-                //Ensure A or B was freshly pressed
-                if ((!button_is_held(A, POST_GAME_LOOP) && button_is_down(A)) || (!button_is_held(B, POST_GAME_LOOP) && button_is_down(B))) {
-
+                // Ensure A or B was freshly pressed
+                if ((!button_is_held(A, POST_GAME_LOOP) && button_is_down(A)) ||
+                    (!button_is_held(B, POST_GAME_LOOP) && button_is_down(B))) {
                     if (counter_difference == 0) {
-
-                        //A or B were pressed together (first button) = BAD
-                        if ((!button_is_held(A, POST_GAME_LOOP) && button_is_down(A)) && (!button_is_held(B, POST_GAME_LOOP) && button_is_down(B))) {
+                        // A or B were pressed together (first button) = BAD
+                        if ((!button_is_held(A, POST_GAME_LOOP) && button_is_down(A)) &&
+                            (!button_is_held(B, POST_GAME_LOOP) && button_is_down(B))) {
                             FIFOQueue::push("hit both A/B as first", Queue, 0x0000FF00);
                             exitCheck = true;
                         } else {
-                            //A or B is pressed here (first button) = GOOD
+                            // A or B is pressed here (first button) = GOOD
                             if (!button_is_held(A, POST_GAME_LOOP) && button_is_down(A))
                                 firstPressedButton = 0;
                             else
@@ -88,42 +86,42 @@ void run() {
                         // B or A was pressed here as second button, success
                         FIFOQueue::push("got UMD", Queue, 0x00CC0000);
                         exitCheck = true;
-                    } else { //We went late
+                    } else {  // We went late
 
-                        //Missed second button
+                        // Missed second button
                         if (firstPressedButton != -1) {
-
-                            //Ensure this is the button that needs to be pressed
-                            if ((firstPressedButton == 0 && button_is_down(B)) || (firstPressedButton == 1 && button_is_down(A))) {
-                                tp_sprintf(buf, "%df late on second %s", counter_difference - 1, getPressedButtonText());
+                            // Ensure this is the button that needs to be pressed
+                            if ((firstPressedButton == 0 && button_is_down(B)) ||
+                                (firstPressedButton == 1 && button_is_down(A))) {
+                                tp_sprintf(buf, "%df late on second %s", counter_difference - 1,
+                                           getPressedButtonText());
                                 FIFOQueue::push(buf, Queue, 0x99000000);
                                 exitCheck = true;
-                            }           
-                        } else { //Missed first button
-                            tp_sprintf(buf, "%df late on first %s", counter_difference, getPressedButtonText());
+                            }
+                        } else {  // Missed first button
+                            tp_sprintf(buf, "%df late on first %s", counter_difference,
+                                       getPressedButtonText());
                             FIFOQueue::push(buf, Queue, 0x99000000);
                             exitCheck = true;
                         }
                     }
-                }
-                else { //Neither A or B was freshly pressed
+                } else {  // Neither A or B was freshly pressed
 
-                    if (counter_difference == 0) { //A or B wasn't pressed on frame 0, we possibly went early
+                    if (counter_difference ==
+                        0) {  // A or B wasn't pressed on frame 0, we possibly went early
 
                         if (lastFrameADown && lastFrameBDown) {
                             FIFOQueue::push("1f early on A/B", Queue, 0x0000FF00);
                             exitCheck = true;
-                        }
-                        else if (lastFrameADown) {
+                        } else if (lastFrameADown) {
                             FIFOQueue::push("1f early on A", Queue, 0x0000FF00);
                             exitCheck = true;
-                        }  
-                        else if (lastFrameBDown) {
+                        } else if (lastFrameBDown) {
                             FIFOQueue::push("1f early on B", Queue, 0x0000FF00);
                             exitCheck = true;
                         }
                     }
-                }             
+                }
             } else {
                 exitCheck = true;
             }
