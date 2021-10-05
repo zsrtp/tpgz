661fca7b14fc311008cf00722646b1e10d293a16
kipcode66 <kipcode66@gmail.com>
2021-10-05 18:31:57 -0400

    switched to provided toolchain

--- cmake/CheckDevkitPro.cmake
+++ cmake/CheckDevkitPro.cmake
@@ -1,26 +1,17 @@
 if(NOT DEFINED ENV{DEVKITPPC} OR NOT DEFINED ENV{DEVKITPRO})
-    message(FATAL_ERROR "Please set DEVKITPPC and DEVKITPRO in your environment. export DEVKITPPC=<path to>devkitPro/devkitPPC")
+    message(FATAL_ERROR "DevkitPro in your system.")
 endif()
 
-set(CMAKE_SYSTEM_NAME "Gecko")
-set(CMAKE_SYSTEM_PROCESSOR powerpc)
-
-set(DEVKITPRO_TOOLS "$ENV{DEVKITPRO}/tools/bin")
-set(DEVKITPPC_BIN "$ENV{DEVKITPRO}/devkitPPC/bin")
-set(DEVKITPPC_PREFIX "${DEVKITPPC_BIN}/powerpc-eabi-")
-
-set(CMAKE_C_COMPILER "${DEVKITPPC_PREFIX}gcc")
-set(CMAKE_CXX_COMPILER "${DEVKITPPC_PREFIX}g++")
-set(CMAKE_C_COMPILER_AR "${DEVKITPPC_PREFIX}gcc-ar")
-
-set(DEVKITPRO_MACHDEP "-DGEKKO -mogc -mcpu=750 -meabi -mhard-float")
+include($ENV{DEVKITPRO}/cmake/devkitPPC.cmake)
 
 if(PLATFORM STREQUAL "GCN")
     set(devkitpro_platform_path "gamecube")
     set(devkitpro_ogclib_path "cube")
+    set(DEVKITPRO_MACHDEP "-DGEKKO -mogc -mcpu=750 -meabi -mhard-float")
 else()
     set(devkitpro_platform_path "wii")
     set(devkitpro_ogclib_path "wii")
+    set(DEVKITPRO_MACHDEP "-DGEKKO -mrvl -mcpu=750 -meabi -mhard-float")
 endif()
 
 set(DEVKITPRO_PORTLIBS_PATH "$ENV{DEVKITPRO}/portlibs")
