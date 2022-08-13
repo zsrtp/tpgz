c99c80af7004640c3c173ebe68b04b40f8fb426c
kipcode66 <kipcode66@gmail.com>
2022-08-13 05:32:38 +0000

    changed libraries optimization level

--- cmake/gcn_c.cmake
+++ cmake/gcn_c.cmake
@@ -1,5 +1,5 @@
 file(GLOB_RECURSE TPGZ_GNC_CPPFILES "${CMAKE_SOURCE_DIR}/external/gcn_c/src/*.c")
 
 add_library(gcn_c STATIC "${TPGZ_GNC_CPPFILES}")
-set_property(TARGET gcn_c PROPERTY COMPILE_FLAGS "-g -c -O2 -Wall ${DEVKITPRO_MACHDEP}")
+set_property(TARGET gcn_c PROPERTY COMPILE_FLAGS "-g -c -Os -Wall ${DEVKITPRO_MACHDEP}")
 target_include_directories(gcn_c PUBLIC "${CMAKE_SOURCE_DIR}/external/gcn_c/include")
