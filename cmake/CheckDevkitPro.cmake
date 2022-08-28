5e0dab759d3acad9675878d0fc59572e95662e1f
kipcode66 <kipcode66@gmail.com>
2022-08-28 17:38:21 +0000

    patch setTag bug

--- cmake/CheckDevkitPro.cmake
+++ cmake/CheckDevkitPro.cmake
@@ -1,15 +1,19 @@
-if(NOT DEFINED ENV{DEVKITPPC} OR NOT DEFINED ENV{DEVKITPRO})
-    message(FATAL_ERROR "DevkitPro in your system.")
+cmake_minimum_required(VERSION 3.16)
+
+if(NOT DEFINED ENV{DEVKITPRO})
+    message(FATAL_ERROR "Could not find DevkitPro in your system.")
 endif()
 
 include($ENV{DEVKITPRO}/cmake/devkitPPC.cmake)
 
 if(PLATFORM STREQUAL "GCN")
+    # include($ENV{DEVKITPRO}/cmake/GameCube.cmake)
     message(STATUS "DevkitPro loaded for Gamecube")
     set(devkitpro_platform_path "gamecube")
     set(devkitpro_ogclib_path "cube")
     set(DEVKITPRO_MACHDEP_LIST -mno-sdata -DGEKKO -mogc -mcpu=750 -meabi -mhard-float)
 else()
+    # include($ENV{DEVKITPRO}/cmake/Wii.cmake)
     message(STATUS "DevkitPro loaded for Wii")
     set(devkitpro_platform_path "wii")
     set(devkitpro_ogclib_path "wii")
