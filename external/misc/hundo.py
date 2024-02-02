29f84824f3ba2ebc30fbf6dbd0a2d0357338c4fd
TakaRikka <takarikka@outlook.com>
2024-02-02 09:28:12 +0000

    update hundo saves

--- external/misc/hundo.py
+++ external/misc/hundo.py
@@ -37,15 +37,14 @@ file_names = [
 	"gm",
 	"dangoro",
 	"post_gm",
-    "eldin_collection",
 	"lakebed_bk_skip",
 	"morpheel",
-	"star_1",
 	"mdh_tower",
 	"mdh_bridge",
 	"post_mdh",
+    "star_1",
+    "kb1", # new
 	"iza_1_skip",
-    "iza_2",
 	"lh_cave",
 	"camp",
 	"ag",
@@ -70,7 +69,7 @@ file_names = [
 	"dot_skip",
 	"post_tot",
 	"hotspring",
-	"silver_rupee",
+    "gorge_arc", # new
 	"ice_puzzle",
 	"hv_archery",
 	"cits_1",
@@ -97,7 +96,7 @@ file_names = [
 	"horseback_ganon",
 ]
 
-hundo_p = [{**default_entry, "id": i, "filename": file_names[i]} for i in range(82)]
+hundo_p = [{**default_entry, "id": i, "filename": file_names[i]} for i in range(81)]
 
 # ordon gate clip
 hundo_p[1]["requirements"] = REQ_POS | REQ_CAM
@@ -139,95 +138,87 @@ hundo_p[20]["angle"] = 27714
 hundo_p[20]["pos"] = -13715.0712, 0.00, -14238.0654
 
 # lakebed bk skip
-hundo_p[25]["requirements"] = REQ_POS | REQ_CAM
-hundo_p[25]["cam"]["target"] = 71.9835968, 1660.0, 2839.01587
-hundo_p[25]["cam"]["pos"] = 71.9835968, 1719.93542, 2969.04565
-hundo_p[25]["angle"] = 32767
-hundo_p[25]["pos"] = 71.9835968, 1500.00, 2839.01587
-hundo_p[25]["counter"] = 30
+hundo_p[24]["requirements"] = REQ_POS | REQ_CAM
+hundo_p[24]["cam"]["target"] = 71.9835968, 1660.0, 2839.01587
+hundo_p[24]["cam"]["pos"] = 71.9835968, 1719.93542, 2969.04565
+hundo_p[24]["angle"] = 32767
+hundo_p[24]["pos"] = 71.9835968, 1500.00, 2839.01587
+hundo_p[24]["counter"] = 30
 
 # mdh tower
-hundo_p[28]["requirements"] = REQ_POS | REQ_CAM
-hundo_p[28]["cam"]["target"] = 25256.7285, -2919.95215, 2839.01587
-hundo_p[28]["cam"]["pos"] = 10193.6064, 25254.7852, -2874.2627
-hundo_p[28]["angle"] = 32025
-hundo_p[28]["pos"] = 25254.6875, -3031.50854, 10222.1445
-hundo_p[28]["counter"] = 15
+hundo_p[26]["requirements"] = REQ_POS | REQ_CAM
+hundo_p[26]["cam"]["target"] = 25256.7285, -2919.95215, 2839.01587
+hundo_p[26]["cam"]["pos"] = 10193.6064, 25254.7852, -2874.2627
+hundo_p[26]["angle"] = 32025
+hundo_p[26]["pos"] = 25254.6875, -3031.50854, 10222.1445
+hundo_p[26]["counter"] = 15
 
 # poe 1 skip
-hundo_p[36]["requirements"] = REQ_POS | REQ_CAM
-hundo_p[36]["cam"]["target"] = -2047.97168, 130.16568, -587.317139
-hundo_p[36]["cam"]["pos"] = -1779.00293, 213.707397, -584.686768
-hundo_p[36]["angle"] = 49030
-hundo_p[36]["pos"] = -2046.97168, 0.0, -587.304871
-hundo_p[36]["counter"] = 10
+hundo_p[35]["requirements"] = REQ_POS | REQ_CAM
+hundo_p[35]["cam"]["target"] = -2047.97168, 130.16568, -587.317139
+hundo_p[35]["cam"]["pos"] = -1779.00293, 213.707397, -584.686768
+hundo_p[35]["angle"] = 49030
+hundo_p[35]["pos"] = -2046.97168, 0.0, -587.304871
+hundo_p[35]["counter"] = 10
 
 # spr superjump
-hundo_p[42]["requirements"] = REQ_POS | REQ_CAM
-hundo_p[42]["cam"]["target"] = 1529.35425, 466.16306, 3684.08252
-hundo_p[42]["cam"]["pos"] = 1765.20581, 691.830688, 3662.42749
-hundo_p[42]["angle"] = 50120
-hundo_p[42]["pos"] = 1530.35, 359.56, 3683.99
-hundo_p[42]["counter"] = 30
+hundo_p[41]["requirements"] = REQ_POS | REQ_CAM
+hundo_p[41]["cam"]["target"] = 1529.35425, 466.16306, 3684.08252
+hundo_p[41]["cam"]["pos"] = 1765.20581, 691.830688, 3662.42749
+hundo_p[41]["angle"] = 50120
+hundo_p[41]["pos"] = 1530.35, 359.56, 3683.99
+hundo_p[41]["counter"] = 30
 
 # spr bk lja
-hundo_p[43]["requirements"] = REQ_POS
-hundo_p[43]["angle"] = 32887
-hundo_p[43]["pos"] = -2171.19, 973.96, -2384.89
-hundo_p[43]["counter"] = 30
+hundo_p[42]["requirements"] = REQ_POS
+hundo_p[42]["angle"] = 32887
+hundo_p[42]["pos"] = -2171.19, 973.96, -2384.89
+hundo_p[42]["counter"] = 30
 
 # grove skip
-hundo_p[47]["requirements"] = REQ_POS
-hundo_p[47]["cam"]["target"] = -9965.82617, 2176.59863, 4084.57056
-hundo_p[47]["cam"]["pos"] = -10415.2363, 2212.92139, 4370.72852
-hundo_p[47]["angle"] = 22306
-hundo_p[47]["pos"] = -9966.6689, 2000.0, 4085.1082
-hundo_p[47]["counter"] = 30
+hundo_p[46]["requirements"] = REQ_POS
+hundo_p[46]["cam"]["target"] = -9965.82617, 2176.59863, 4084.57056
+hundo_p[46]["cam"]["pos"] = -10415.2363, 2212.92139, 4370.72852
+hundo_p[46]["angle"] = 22306
+hundo_p[46]["pos"] = -9966.6689, 2000.0, 4085.1082
+hundo_p[46]["counter"] = 30
 
 # grove boost
-hundo_p[48]["requirements"] = REQ_POS
-hundo_p[48]["cam"]["target"] = -1765.32605, 1180.38452, 4303.98584
-hundo_p[48]["cam"]["pos"] = -1368.4314, 1347.8916, 4057.84863
-hundo_p[48]["angle"] = 54947
-hundo_p[48]["pos"] = -1764.4763, 1000.0, 4303.4585
-hundo_p[48]["counter"] = 30
+hundo_p[47]["requirements"] = REQ_POS
+hundo_p[47]["cam"]["target"] = -1765.32605, 1180.38452, 4303.98584
+hundo_p[47]["cam"]["pos"] = -1368.4314, 1347.8916, 4057.84863
+hundo_p[47]["angle"] = 54947
+hundo_p[47]["pos"] = -1764.4763, 1000.0, 4303.4585
+hundo_p[47]["counter"] = 30
 
 # dot skip
-hundo_p[54]["requirements"] = REQ_POS | REQ_CAM
-hundo_p[54]["cam"]["target"] = 1361.59766, -33.1954155, -1090.47632
-hundo_p[54]["cam"]["pos"] = 1396.36316, 9.51973343, -719.644531
-hundo_p[54]["angle"] = 33673
-hundo_p[54]["pos"] = 1361.68408, -143.56076, -1089.4801
-
-# silver rupee
-hundo_p[57]["requirements"] = REQ_POS | REQ_CAM
-hundo_p[57]["cam"]["target"] = 961.308044, 203.885788, 4184.82471
-hundo_p[57]["cam"]["pos"] = 1220.0835, 320.043884, 4269.48779
-hundo_p[57]["angle"] = 45836
-hundo_p[57]["pos"] = 962.257813, 100.0, 4185.1377
-hundo_p[57]["counter"] = 30
+hundo_p[53]["requirements"] = REQ_POS | REQ_CAM
+hundo_p[53]["cam"]["target"] = 1361.59766, -33.1954155, -1090.47632
+hundo_p[53]["cam"]["pos"] = 1396.36316, 9.51973343, -719.644531
+hundo_p[53]["angle"] = 33673
+hundo_p[53]["pos"] = 1361.68408, -143.56076, -1089.4801
 
 # hv archery
-hundo_p[59]["requirements"] = REQ_POS
-hundo_p[59]["angle"] = 64520
-hundo_p[59]["pos"] = 3125.57, -62.16, -9360.22
-hundo_p[59]["counter"] = 30
+hundo_p[58]["requirements"] = REQ_POS
+hundo_p[58]["angle"] = 64520
+hundo_p[58]["pos"] = 3125.57, -62.16, -9360.22
+hundo_p[58]["counter"] = 30
 
 # city 1
-hundo_p[60]["requirements"] = REQ_POS | REQ_CAM
-hundo_p[60]["cam"]["target"] = 1313.54285, -234.203003, 5545.16846
-hundo_p[60]["cam"]["pos"] = 1027.53259, -108.096123, 5605.23047
-hundo_p[60]["angle"] = 16384
-hundo_p[60]["pos"] = 1309.60645, -240.0, 5533.43848
-hundo_p[60]["counter"] = 10
+hundo_p[59]["requirements"] = REQ_POS | REQ_CAM
+hundo_p[59]["cam"]["target"] = 1313.54285, -234.203003, 5545.16846
+hundo_p[59]["cam"]["pos"] = 1027.53259, -108.096123, 5605.23047
+hundo_p[59]["angle"] = 16384
+hundo_p[59]["pos"] = 1309.60645, -240.0, 5533.43848
+hundo_p[59]["counter"] = 10
 
 # cats
-hundo_p[76]["requirements"] = REQ_POS | REQ_CAM
-hundo_p[76]["cam"]["target"] = 5309.32373, 160.1, -3581.83423
-hundo_p[76]["cam"]["pos"] = 4893.25391, 160.117676, -3524.51245
-hundo_p[76]["angle"] = 17282
-hundo_p[76]["pos"] = 5238.59, 0.00, -3575.74
-hundo_p[76]["counter"] = 30
+hundo_p[75]["requirements"] = REQ_POS | REQ_CAM
+hundo_p[75]["cam"]["target"] = 5309.32373, 160.1, -3581.83423
+hundo_p[75]["cam"]["pos"] = 4893.25391, 160.117676, -3524.51245
+hundo_p[75]["angle"] = 17282
+hundo_p[75]["pos"] = 5238.59, 0.00, -3575.74
+hundo_p[75]["counter"] = 30
 
 file = open("hundo.bin", "wb")
 
