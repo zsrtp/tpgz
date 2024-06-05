094fa6225240cd8cb4f05fd6021dfb327792a832
kipcode66 <kipcode66@gmail.com>
2024-06-05 22:24:11 +0000

    fix memory leak

--- modules/features/umd/src/main.cpp
+++ modules/features/umd/src/main.cpp
@@ -7,7 +7,8 @@ void main() {
     GZFlg_addFlag(new GZFlag{GZFLG_UMD, &g_tools[UMD_INDEX].active, POST_GAME_LOOP, UMDIndicator::execute});
 }
 void exit() {
-    GZFlg_removeFlag(GZFLG_UMD);
+    auto* flg = GZFlg_removeFlag(GZFLG_UMD);
+    delete flg;
 }
 
 }  // namespace tpgz::modules
