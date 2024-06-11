95a74c27ecd060b6081b19c63bae5c4f1eec3ff9
kipcode66 <kipcode66@gmail.com>
2024-06-11 04:05:00 +0000

    updated menu offsets for wii & added indicator for watches menu

--- modules/menus/menu_credits/src/credits_menu.cpp
+++ modules/menus/menu_credits/src/credits_menu.cpp
@@ -24,8 +24,11 @@ void CreditsMenu::draw() {
         load_texture("/tpgz/tex/heart.tex", &l_creditsHeart);
     }
 
+    float scale = isWidescreen ? 0.75f : 1.0f;
+    float offset = isWidescreen ? 100.0f : 0.0f;
+
     if (l_creditsHeart.loadCode == TexCode::TEX_OK) {
-            Draw::drawRect(0xFFFFFFFF, {315.0f, 65.0f},{30 * (isWidescreen ? 0.75f : 1.0f), 30}, &l_creditsHeart._texObj);
+            Draw::drawRect(0xFFFFFFFF, {offset + 315.0f * scale, 65.0f},{30 * scale, 30}, &l_creditsHeart._texObj);
         }
 
     if (GZ_getButtonTrig(BACK_BUTTON)) {
@@ -33,22 +36,21 @@ void CreditsMenu::draw() {
         return;
     }
 
-    
-    Font::GZ_drawStr("tpgz - the twilight princess practice rom", 25.0f, 60.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 25.0f);
-    Font::GZ_drawStr("made with", 175.0f, 90.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 25.0f);
-    Font::GZ_drawStr("by", 360.0f, 90.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 25.0f);
+    Font::GZ_drawStr("tpgz - the twilight princess practice rom", offset + 25.0f * scale, 60.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 25.0f);
+    Font::GZ_drawStr("made with", offset + 175.0f * scale, 90.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 25.0f);
+    Font::GZ_drawStr("by", offset + 360.0f * scale, 90.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 25.0f);
 
-    Font::GZ_drawStr("kipcode", 135.0f, 110.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 20.0f);  
-    Font::GZ_drawStr("pheenoh", 265.0f, 110.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 20.0f);
-    Font::GZ_drawStr("taka", 390.0f, 110.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 20.0f);
+    Font::GZ_drawStr("kipcode", offset + 135.0f * scale, 110.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 20.0f);  
+    Font::GZ_drawStr("pheenoh", offset + 265.0f * scale, 110.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 20.0f);
+    Font::GZ_drawStr("taka", offset + 390.0f * scale, 110.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 20.0f);
 
-    Font::GZ_drawStr("special thanks", 175.0f, 170.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 25.0f);
-    Font::GZ_drawStr("tp decomp team", 85.0f, 190.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 17.0f);
-    Font::GZ_drawStr("tp speedrun community", 345.0f, 190.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 17.0f);
-    Font::GZ_drawStr("c_midnight", 85.0f, 210.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 17.0f);
-    Font::GZ_drawStr("cryze", 245.0f, 210.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 17.0f);
-    Font::GZ_drawStr("jdflyer", 345.0f, 210.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 17.0f);
-    Font::GZ_drawStr("icogn", 85.0f, 230.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 17.0f);
-    Font::GZ_drawStr("lunarsoap", 245.0f, 230.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 17.0f);
-    Font::GZ_drawStr("zephiles", 345.0f, 230.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 17.0f);
+    Font::GZ_drawStr("special thanks", offset + 175.0f * scale, 170.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 25.0f);
+    Font::GZ_drawStr("tp decomp team", offset + 85.0f * scale, 190.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 17.0f);
+    Font::GZ_drawStr("tp speedrun community", offset + 345.0f * scale, 190.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 17.0f);
+    Font::GZ_drawStr("c_midnight", offset + 85.0f * scale, 210.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 17.0f);
+    Font::GZ_drawStr("cryze", offset + 245.0f * scale, 210.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 17.0f);
+    Font::GZ_drawStr("jdflyer", offset + 345.0f * scale, 210.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 17.0f);
+    Font::GZ_drawStr("icogn", offset + 85.0f * scale, 230.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 17.0f);
+    Font::GZ_drawStr("lunarsoap", offset + 245.0f * scale, 230.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 17.0f);
+    Font::GZ_drawStr("zephiles", offset + 345.0f * scale, 230.0f, 0xFFFFFFFF, GZ_checkDropShadows(), 17.0f);
 }
