ab54d91ac24c792e03d4f687983cb8518faed23b
kipcode66 <kipcode66@gmail.com>
2024-06-17 15:25:46 +0000

    moved one time patch code to init module

--- modules/init/src/main.cpp
+++ modules/init/src/main.cpp
@@ -20,8 +20,10 @@
 #include "events/draw_listener.h"
 #include "events/pre_loop_listener.h"
 #include "events/post_loop_listener.h"
+#include "libtp_c/include/dolphin/os/OSCache.h"
 
 void GZ_PosSettings_initDefaults();
+void GZ_patchLinkColor();
 
 namespace tpgz::modules {
 void main() {
@@ -125,4 +127,38 @@ void GZ_PosSettings_initDefaults() {
     GZStng_addSetting(STNG_SPRITES_HEAP_INFO, new Vec2{145.0f, 25.0f}, sizeof(Vec2));
     GZStng_addSetting(STNG_SPRITES_MASH_INFO, new Vec2{450.0f, 400.0f}, sizeof(Vec2));
     GZStng_addSetting(STNG_SPRITES_TRANSFORM_IND, new Vec2{465.0f, 30.0f}, sizeof(Vec2));
+}
+
+inline void GZ_patchLinkColor() {
+#ifdef WII_PLATFORM
+#ifdef WII_NTSCU_10
+#define SWORD_UP_RED_ADDR 0x801176B0
+#define SWORD_UP_GREEN_ADDR 0x801176C8
+#define SWORD_UP_BLUE_ADDR 0x801176E0
+#endif
+#ifdef WII_NTSCU_12
+#define SWORD_UP_RED_ADDR 0x80117de0
+#define SWORD_UP_GREEN_ADDR 0x80117df8
+#define SWORD_UP_BLUE_ADDR 0x80117e10
+#endif
+#ifdef WII_NTSCJ
+#define SWORD_UP_RED_ADDR 0x80117bec
+#define SWORD_UP_GREEN_ADDR 0x80117c04
+#define SWORD_UP_BLUE_ADDR 0x80117c1c
+#endif
+#ifdef WII_PAL
+#define SWORD_UP_RED_ADDR 0x80117d64
+#define SWORD_UP_GREEN_ADDR 0x80117d7c
+#define SWORD_UP_BLUE_ADDR 0x80117d94
+#endif
+    *reinterpret_cast<uint32_t*>(SWORD_UP_RED_ADDR) = 0x60000000;  // nop
+    DCFlushRange((void*)(SWORD_UP_RED_ADDR), sizeof(uint32_t));
+    ICInvalidateRange((void*)(SWORD_UP_RED_ADDR), sizeof(uint32_t));
+    *reinterpret_cast<uint32_t*>(SWORD_UP_GREEN_ADDR) = 0x60000000;  // nop
+    DCFlushRange((void*)(SWORD_UP_GREEN_ADDR), sizeof(uint32_t));
+    ICInvalidateRange((void*)(SWORD_UP_GREEN_ADDR), sizeof(uint32_t));
+    *reinterpret_cast<uint32_t*>(SWORD_UP_BLUE_ADDR) = 0x60000000;  // nop
+    DCFlushRange((void*)(SWORD_UP_BLUE_ADDR), sizeof(uint32_t));
+    ICInvalidateRange((void*)(SWORD_UP_BLUE_ADDR), sizeof(uint32_t));
+#endif  // WII_PLATFORM
 }
