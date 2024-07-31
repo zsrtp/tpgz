dac181e03b92c1ecfe7978ce1ee64d0bbcafe795
kipcode66 <kipcode66@gmail.com>
2024-07-30 22:57:59 -0400

    replaced patcher and removed dummy library (#84)
    Co-authored-by: Pheenoh <pheenoh@gmail.com>

--- cmake/WII_NTSCU_12.cmake
+++ cmake/WII_NTSCU_12.cmake
@@ -10,7 +10,7 @@ set(TPGZ_CFG_LOADER_ADDR "0x80005BF4")
 set(TPGZ_CFG_SETTAG_ADDR "0x80226318")
 
 # TOML values
-set(TPGZ_CFG_SRC_ISO "${CMAKE_SOURCE_DIR}/isos/RZDE01_2.iso")
+set(TPGZ_CFG_SRC_ISO "RZDE01_2.iso")
 set(TPGZ_CFG_SRC_MAP "map/Rfinal/Release/RframeworkF.map")
 set(TPGZ_CFG_BLD_ISO "tpgzw.iso")
 set(TPGZ_CFG_BLD_MAP "RZDE01_2.map")
--- cmake/patcher.cmake
+++ cmake/patcher.cmake
@@ -1,6 +1,4 @@
-string(TOLOWER ${PLATFORM} TPGZ_PATCHER_DIR)
-
-find_program(TPGZ_PATCHER_EXE romhack HINTS ${CMAKE_SOURCE_DIR}/bin/${TPGZ_PATCHER_DIR})
+find_program(TPGZ_PATCHER_EXE romhack HINTS ${CMAKE_SOURCE_DIR}/bin)
 
 if(NOT TPGZ_PATCHER_EXE)
     message(FATAL_ERROR "please put \"romhack\" in the bin/ folder")
